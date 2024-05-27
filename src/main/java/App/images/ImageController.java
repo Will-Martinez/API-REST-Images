3600b71dd9bb9cef66bb2c67131e561bafd196ce
Will-Martinez <willandersonmartinez@gmail.com>
2024-05-27 14:46:34 -0300

    Improove code to use dependency injection and inversion of control.

--- src/main/java/App/images/ImageController.java
+++ src/main/java/App/images/ImageController.java
@@ -1,20 +1,24 @@
 package App.images;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/images")
 public class ImageController {
+
+    private final ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
 
     @GetMapping("/")
     public String coreApp() {
         return "Hello World, this's the core App";
     }
 
-    @PostMapping("/api/images/handleImage")
+    @PostMapping("/handleImage")
     public String handleImage(@RequestBody String base64) {
-        return "Here i handle my images";
+        return imageService.createImage(base64);
     }
 }
---
+++ src/main/java/App/images/ImageService.java
@@ -0,0 +1,11 @@
+package App.images;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+    public String createImage (String base64) {
+        return base64;
+    }
+}
